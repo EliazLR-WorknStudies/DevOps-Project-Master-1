a485019f7f04fb4ac98b640a4b5fa3ef3706d7d5
Eliaz <47760515+Eliaz-LR@users.noreply.github.com>
2024-03-13 16:58:35 +0100

    NEXTCLOUD MARCHE BORDEL

--- terraform/domain.tf
+++ terraform/domain.tf
@@ -1,4 +1,12 @@
 # Add a record to a sub-domain
+resource "ovh_domain_zone_record" "all_eliaz" {
+  zone      = "uca-devops.ovh"
+  subdomain = "*.eliaz"
+  fieldtype = "A"
+  ttl       = 60
+  target    = local.external_ip
+}
+
 resource "ovh_domain_zone_record" "eliaz" {
   zone      = "uca-devops.ovh"
   subdomain = "eliaz"
