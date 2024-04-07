afbddd49f7f847220b358333a54875ea7622783a
Eliaz <47760515+Eliaz-LR@users.noreply.github.com>
2024-04-07 09:00:07 +0200

    :bug: Secret retreiving fix + traefik

--- init.sh
+++ init.sh
@@ -1,22 +1,11 @@
 # Login to Vault using token (do not change this line)
 VAULT_TOKEN=$(curl -s -X POST -k --data "{\"token\":\"$CI_JOB_TOKEN\"}" "https://vault.edu.forestier.re/v1/auth/gitlab/ci" | python3 jq.py "auth/client_token")
 
-echo "Vault token retrieved"
-echo "VAULT TOKEN: $VAULT_TOKEN"
 
-# Retrieve a Vault secret and store it in a variable. here, chagne {ENGINE} and {PATH} to fit your needs
-# Example : https://vault.edu.forestier.re/v1/home/data/fforestier/test
+REGISTRY=$(curl -s -k --header "X-Vault-Token: $VAULT_TOKEN"  --request GET https://vault.edu.forestier.re/v1/gitlab/data/public/registry)
 
-
-
-REGISTRY_USERNAME=$(curl -s -k --header "X-Vault-Token: $VAULT_TOKEN"  --request GET https://vault.edu.forestier.re/v1/gitlab/data/public/registry/username)
-REGISTRY_PASSWORD=$(curl -s -X GET -k --header "X-Vault-Token: $VAULT_TOKEN" "https://vault.edu.forestier.re/v1/gitlab/data/registry/password")
-
-echo "Secrets retrieved from Vault"
-echo "REGISTRY_USERNAME: $REGISTRY_USERNAME"
-echo "REGISTRY_PASSWORD: $REGISTRY_PASSWORD"
 
 # Here, export value "A" of our secret to "A" in our environment (change "A" to your needs...)
-export USERNAME=$(echo "$REGISTRY_USERNAME")
-export PASSWORD=$(echo "$REGISTRY_PASSWORD")
+export USERNAME=$(echo "$REGISTRY" | jq -r 'data/data/username')
+export PASSWORD=$(echo "$REGISTRY" | jq -r '.data/data/password')
 
